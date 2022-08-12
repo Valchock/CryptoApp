07c2bf423f54418ce3b70d2e6aa5bec26cd405c5
valli-11189 <vallimaiel.cmr@zohocorp.com>
2022-08-12 19:16:31 +0530

    Added Compose Dependency and TabLayout

--- app/src/main/java/com/example/cryptoapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/ui/MainActivity.kt
@@ -2,9 +2,25 @@ package com.example.cryptoapp.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
-import com.example.cryptoapp.R
 import com.example.cryptoapp.presentation.viewModel.CryptoExchangeViewModel
+import com.example.cryptoapp.utils.*
+import com.google.accompanist.pager.*
+import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,8 +29,93 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         viewModel = ViewModelProvider(this)[CryptoExchangeViewModel::class.java]
         viewModel.syncExchangeInfo()
+        setContent {
+            TabLayout()
+
+        }
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun TabLayout() {
+    val pagerState = rememberPagerState(pageCount = 4)
+    Column() {
+        Tabs(pagerState = pagerState)
+        TabsContent(pagerState = pagerState)
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun Tabs(pagerState: PagerState) {
+    val list = listOf(
+        "INR",
+        "USDT",
+        "WRX",
+        "BTC"
+    )
+    val scope = rememberCoroutineScope()
+    TabRow(
+        selectedTabIndex = pagerState.currentPage,
+        backgroundColor = LightGray,
+        contentColor = Black,
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(
+                Modifier.pagerTabIndicatorOffset(pagerState, tabPositions),
+                height = 2.dp,
+                color = DarkBlue
+            )
+        }
+    ) {
+        list.forEachIndexed { index, _ ->
+            Tab(
+                text = {
+                    Text(
+                        list[index],
+                        color = if (pagerState.currentPage == index) Blue else Black
+                    )
+                },
+                selected = pagerState.currentPage == index,
+                onClick = {
+                    scope.launch {
+                        pagerState.animateScrollToPage(index)
+                    }
+                }
+            )
+        }
+    }
+}
+
+@ExperimentalPagerApi
+@Composable
+fun TabsContent(pagerState: PagerState) {
+    HorizontalPager(state = pagerState) { page ->
+        when (page) {
+            0 -> TabContentScreen(data = "INR")
+            1 -> TabContentScreen(data = "USDT")
+            2 -> TabContentScreen(data = "WRX")
+            3 -> TabContentScreen(data = "BTC")
+        }
+    }
+}
+
+
+@Composable
+fun TabContentScreen(data: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = data,
+            style = MaterialTheme.typography.h5,
+            color = DarkGray,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
     }
 }
---
+++ app/src/main/java/com/example/cryptoapp/utils/Color.kt
@@ -0,0 +1,11 @@
+package com.example.cryptoapp.utils
+
+import androidx.compose.ui.graphics.Color
+
+val BlueGray = Color(0xFFCFD8DC)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val Blue = Color(0xFF2196F3)
+val DarkBlue = Color(0xFF1976D2)
+val LightGray = Color(0xFFEEEEEE)
+val DarkGray = Color(0xFFE0E0E0)
